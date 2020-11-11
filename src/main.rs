dc08fc586e4e7d87e1f16b426175043e59172dd0
picania <mangustspam@gmail.com>
2020-11-11 11:14:49 +0700

    Глава 2: Объект Vec3

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,323 @@
+use std::ops::Add;
+use std::ops::AddAssign;
+use std::ops::Div;
+use std::ops::DivAssign;
+use std::ops::Index;
+use std::ops::IndexMut;
+use std::ops::Mul;
+use std::ops::MulAssign;
+use std::ops::Neg;
+use std::ops::Sub;
+use std::ops::SubAssign;
+use std::fmt::{Display, Formatter, Result};
+
+/// Преобразует цветовые компоненты пикселя к [`u8`] и печатает на экран.
+///
+/// [`u8`]: https://doc.rust-lang.org/std/primitive.u8.html
+fn render_pixel<T>(pixel: T)
+where
+    T: Color,
+{
+    let ir = (255.99 * pixel.r()) as i32;
+    let ig = (255.99 * pixel.g()) as i32;
+    let ib = (255.99 * pixel.b()) as i32;
+
+    println!("{} {} {}", ir, ig, ib);
+}
+
+/// Трехкомпонентный вектор с плавающей точкой.
+#[derive(Copy, Clone, Debug)]
+pub struct Vec3([f32; 3]);
+
+impl Vec3 {
+    /// Создает новый вектор.
+    pub fn new(x: f32, y: f32, z: f32) -> Self {
+        Self { 0: [x, y, z] }
+    }
+
+    /// Возвращает длину вектора.
+    pub fn length(&self) -> f32 {
+        self.square_length().sqrt()
+    }
+
+    /// Возвращает квадрат длины вектора.
+    pub fn square_length(&self) -> f32 {
+        self.0[0] * self.0[0] + self.0[1] * self.0[1] + self.0[2] * self.0[2]
+    }
+
+    /// Приводит вектор к единичному виду.
+    pub fn make_unit_vector(&mut self) {
+        let k = 1.0 / self.length();
+        *self *= k;
+    }
+}
+
+/// Конструктор по умолчанию.
+impl Default for Vec3 {
+    fn default() -> Self {
+        Vec3 { 0: [0.0, 0.0, 0.0] }
+    }
+}
+
+/// Форматированный вывод.
+impl Display for Vec3 {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        write!(f, "{} {} {}", self.0[0], self.0[1], self.0[2])
+    }
+}
+
+impl PartialEq for Vec3 {
+    fn eq(&self, other: &Self) -> bool {
+        self.0[0] == other.0[0] && self.0[1] == other.0[1] && self.0[2] == other.0[2]
+    }
+}
+
+/// Унарный минус.
+///
+/// Применяет унарный минус к каждой компоненте вектора.
+impl Neg for Vec3 {
+    type Output = Vec3;
+
+    fn neg(self) -> Self::Output {
+        Vec3 {
+            0: [-self.0[0], -self.0[1], -self.0[2]],
+        }
+    }
+}
+
+/// Оператор чтения по индексу.
+impl Index<usize> for Vec3 {
+    type Output = f32;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        &self.0[index]
+    }
+}
+
+/// Оператор записи по индексу.
+impl IndexMut<usize> for Vec3 {
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        &mut self.0[index]
+    }
+}
+
+/// Оператор сложения векторов.
+///
+/// Векторы складываются по компонентам.
+impl Add for Vec3 {
+    type Output = Vec3;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self {
+            0: [
+                self.0[0] + rhs.0[0],
+                self.0[1] + rhs.0[1],
+                self.0[2] + rhs.0[2],
+            ],
+        }
+    }
+}
+
+/// Оператор сложения векторов с присваиванием.
+impl AddAssign for Vec3 {
+    fn add_assign(&mut self, rhs: Self) {
+        self.0[0] += rhs.0[0];
+        self.0[1] += rhs.0[1];
+        self.0[2] += rhs.0[2];
+    }
+}
+
+/// Оператор вычитания векторов.
+///
+/// Векторы вычитаются по компонентам.
+impl Sub for Vec3 {
+    type Output = Vec3;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Self {
+            0: [
+                self.0[0] - rhs.0[0],
+                self.0[1] - rhs.0[1],
+                self.0[2] - rhs.0[2],
+            ],
+        }
+    }
+}
+
+/// Оператор вычитания вектора с присваиванием.
+impl SubAssign for Vec3 {
+    fn sub_assign(&mut self, rhs: Self) {
+        self.0[0] -= rhs.0[0];
+        self.0[1] -= rhs.0[1];
+        self.0[2] -= rhs.0[2];
+    }
+}
+
+/// Оператор умножения векторов.
+///
+/// Векторы умножаются по компонентам.
+impl Mul for Vec3 {
+    type Output = Vec3;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        Self {
+            0: [
+                self.0[0] * rhs.0[0],
+                self.0[1] * rhs.0[1],
+                self.0[2] * rhs.0[2],
+            ],
+        }
+    }
+}
+
+/// Оператор умножения вектора на скаляр.
+///
+/// Каждая компонента вектора умножается на число.
+impl Mul<f32> for Vec3 {
+    type Output = Vec3;
+
+    fn mul(mut self, rhs: f32) -> Self::Output {
+        self *= rhs;
+        self
+    }
+}
+
+/// Оператор умножения скаляра на вектор.
+///
+/// Каждая компонента вектора умножается на число.
+impl Mul<Vec3> for f32 {
+    type Output = Vec3;
+
+    fn mul(self, rhs: Vec3) -> Self::Output {
+        rhs * self
+    }
+}
+
+/// Оператор умножения на вектор с присваиванием.
+impl MulAssign for Vec3 {
+    fn mul_assign(&mut self, rhs: Self) {
+        self.0[0] *= rhs.0[0];
+        self.0[1] *= rhs.0[1];
+        self.0[2] *= rhs.0[2];
+    }
+}
+
+/// Оператор деления векторов.
+///
+/// Компоненты первого вектора делятся на соответствующие компоненты второго вектора.
+impl Div for Vec3 {
+    type Output = Vec3;
+
+    fn div(self, rhs: Self) -> Self::Output {
+        Self {
+            0: [
+                self.0[0] / rhs.0[0],
+                self.0[1] / rhs.0[1],
+                self.0[2] / rhs.0[2],
+            ],
+        }
+    }
+}
+
+/// Оператор деления вектора на скаляр.
+///
+/// Каждая компонента вектора делится на число.
+impl Div<f32> for Vec3 {
+    type Output = Vec3;
+
+    fn div(mut self, rhs: f32) -> Self::Output {
+        self /= rhs;
+        self
+    }
+}
+
+/// Оператор деления на вектор с присваиванием.
+impl DivAssign for Vec3 {
+    fn div_assign(&mut self, rhs: Self) {
+        self.0[0] /= rhs.0[0];
+        self.0[1] /= rhs.0[1];
+        self.0[2] /= rhs.0[2];
+    }
+}
+
+/// Оператор умножения на скаляр с присваиванием.
+///
+/// Каждая компонента вектора умножается на число.
+impl MulAssign<f32> for Vec3 {
+    fn mul_assign(&mut self, rhs: f32) {
+        self.0[0] *= rhs;
+        self.0[1] *= rhs;
+        self.0[2] *= rhs;
+    }
+}
+
+/// Оператор деления на скаляр с присваиванием.
+///
+/// Каждая компонента вектора делится на число.
+#[allow(clippy::suspicious_op_assign_impl)]
+impl DivAssign<f32> for Vec3 {
+    fn div_assign(&mut self, rhs: f32) {
+        let k = 1.0 / rhs;
+
+        self.0[0] *= k;
+        self.0[1] *= k;
+        self.0[2] *= k;
+    }
+}
+
+/// Скалярное произведение двух векторов. [Подробнее].
+///
+/// [Подробнее]: https://ru.wikipedia.org/wiki/%D0%A1%D0%BA%D0%B0%D0%BB%D1%8F%D1%80%D0%BD%D0%BE%D0%B5_%D0%BF%D1%80%D0%BE%D0%B8%D0%B7%D0%B2%D0%B5%D0%B4%D0%B5%D0%BD%D0%B8%D0%B5
+fn dot(a: Vec3, b: Vec3) -> f32 {
+    a.0[0] * b.0[0] + a.0[1] * b.0[1] + a.0[2] * b.0[2]
+}
+
+/// Векторное произведение двух векторов. [Подробнее].
+///
+/// [Подробнее]: https://ru.wikipedia.org/wiki/%D0%92%D0%B5%D0%BA%D1%82%D0%BE%D1%80%D0%BD%D0%BE%D0%B5_%D0%BF%D1%80%D0%BE%D0%B8%D0%B7%D0%B2%D0%B5%D0%B4%D0%B5%D0%BD%D0%B8%D0%B5
+fn cross(a: Vec3, b: Vec3) -> Vec3 {
+    Vec3 {
+        0: [
+            a.0[1] * b.0[2] - a.0[2] * b.0[1],
+            -a.0[0] * b.0[2] - a.0[2] * b.0[0],
+            a.0[0] * b.0[1] - a.0[1] * b.0[0],
+        ],
+    }
+}
+
+/// Создает единичный вектор.
+fn unit_vector(a: Vec3) -> Vec3 {
+    a / a.length()
+}
+
+/// Типаж для описания вектора как точки в пространстве с координатами `x`, `y` и `z`.
+trait Position {
+    fn x(&self) -> f32;
+    fn y(&self) -> f32;
+    fn z(&self) -> f32;
+}
+
+/// Типаж для описания вектора как цвета с компонентами `r`, `g` и `b`.
+trait Color {
+    fn r(&self) -> f32;
+    fn g(&self) -> f32;
+    fn b(&self) -> f32;
+}
+
+impl Color for Vec3 {
+    fn r(&self) -> f32 {
+        self.0[0]
+    }
+
+    fn g(&self) -> f32 {
+        self.0[1]
+    }
+
+    fn b(&self) -> f32 {
+        self.0[2]
+    }
+}
+
 fn main() {
     let nx = 200;
     let ny = 100;
@@ -7,18 +327,12 @@ fn main() {
     println!("{} {}", nx, ny);
     println!("255");
 
-    // Выводим остальные пиксели построчно начиная с верхнего левого угла изображения.
+    // Выводим остальные пиксели построчно начиная с верхнего левого угла изображения
     for y in 0..ny {
         for x in 0..nx {
-            let r = x as f32 / nx as f32;
-            let g = (ny - y) as f32 / ny as f32;
-            let b = 0.2f32;
+            let pixel = Vec3::new(x as f32 / nx as f32, (ny - y) as f32 / ny as f32, 0.2f32);
 
-            let ir = (255.99 * r) as i32;
-            let ig = (255.99 * g) as i32;
-            let ib = (255.99 * b) as i32;
-
-            println!("{} {} {}", ir, ig, ib);
+            render_pixel(pixel);
         }
     }
 }
