005d11192303c84614d522cedb28e391f3572068
picania <mangustspam@gmail.com>
2020-11-11 11:15:22 +0700

    Глава 3: Лучи, простая камера и задний фон

--- src/main.rs
+++ src/main.rs
@@ -285,6 +285,12 @@ fn cross(a: Vec3, b: Vec3) -> Vec3 {
     }
 }
 
+impl From<[f32; 3]> for Vec3 {
+    fn from(a: [f32; 3]) -> Self {
+        Self {0: a}
+    }
+}
+
 /// Создает единичный вектор.
 fn unit_vector(a: Vec3) -> Vec3 {
     a / a.length()
@@ -318,6 +324,60 @@ impl Color for Vec3 {
     }
 }
 
+impl Position for Vec3 {
+    fn x(&self) -> f32 {
+        self.0[0]
+    }
+
+    fn y(&self) -> f32 {
+        self.0[1]
+    }
+
+    fn z(&self) -> f32 {
+        self.0[2]
+    }
+}
+
+/// Луч, направленный из точки `from` в точку `to`.
+pub struct Ray {
+    from: Vec3,
+    to: Vec3,
+}
+
+impl Ray {
+    /// Создает луч с началом в точке `from` и направленный в точку `to`.
+    fn new(from: Vec3, to: Vec3) -> Self {
+        Self {from, to}
+    }
+
+    /// Координаты точки, из которой исходит луч.
+    fn origin(&self) -> Vec3 {
+        self.from
+    }
+
+    /// Координаты точки, куда направлен луч.
+    fn direction(&self) -> Vec3 {
+        self.to
+    }
+
+    /// Координаты точки, лежащей на луче в отрезке `[from; to]`.
+    ///
+    /// Параметр `t` принимает значения в диапазоне `[0; 1]`.
+    /// При `t = 0` точка совпадает с началом отрезка, при `t = 1` точка совпадает с концом отрезка.
+    fn point_at_parameter(&self, t: f32) -> Vec3 {
+        self.from + t * self.to
+    }
+}
+
+fn background(ray: &Ray) -> Vec3 {
+    let unit_direction = unit_vector(ray.direction());
+    let t = 0.5 * (unit_direction.y() + 1.0);
+    let white: Vec3 = [1.0, 1.0, 1.0].into();
+    let light_blue: Vec3 = [0.5, 0.7, 1.0].into();
+
+    (1.0 - t) * white + t * light_blue
+}
+
 fn main() {
     let nx = 200;
     let ny = 100;
@@ -327,12 +387,20 @@ fn main() {
     println!("{} {}", nx, ny);
     println!("255");
 
+    let lower_left_corner: Vec3 = [-2.0, -1.0, -1.0].into();
+    let horizontal: Vec3 = [4.0, 0.0, 0.0].into();
+    let vertical: Vec3 = [0.0, 2.0, 0.0].into();
+    let from: Vec3 = [0.0, 0.0, 0.0].into();
+
     // Выводим остальные пиксели построчно начиная с верхнего левого угла изображения
     for y in 0..ny {
         for x in 0..nx {
-            let pixel = Vec3::new(x as f32 / nx as f32, (ny - y) as f32 / ny as f32, 0.2f32);
+            let u = x as f32 / nx as f32;
+            let v = (ny - y) as f32 / ny as f32;
+            let to = lower_left_corner + u * horizontal + v * vertical;
+            let ray = Ray::new(from, to);
 
-            render_pixel(pixel);
+            render_pixel(background(&ray));
         }
     }
 }
